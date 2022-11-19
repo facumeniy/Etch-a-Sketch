9afef844bb53148d317e3672970e4ffbeb3a3dc7
Facundo Meniy <facundomeniy@gmail.com>
2022-11-18 21:35:47 -0300

    Added basic behaviour of the game

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const grid = document.querySelector("#main-grid");
+const drawBtn = document.querySelector("#draw-btn");
+let click = false;
 
 function createGrid(num){
-    for(let i=0; i<num*num; i++){
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("square");
-        grid.appendChild(newDiv);
+    for(let i=0; i<num; i++){
+        const newRow = document.createElement("div");
+        newRow.classList.add("row");
+        for(let i=0; i<num; i++){
+            const newSquare = document.createElement("div");
+            newSquare.classList.add("square");
+            newRow.appendChild(newSquare);
+            newSquare.addEventListener('mouseover', colorSquare);
+        }
+        grid.appendChild(newRow);
     }
 };
 
-createGrid(2);
+createGrid(64);
+
+function colorSquare(){
+    if(click){
+        this.classList.add("draw");
+    }
+}
+
+grid.addEventListener('mousedown', ()=>{
+    click = !click;
+});
